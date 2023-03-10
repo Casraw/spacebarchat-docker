bb5efa7fccc620627e9e88ccac0f0dc54574abe0
Casraw <casraw70@googlemail.com>
2023-03-10 22:39:35 +0100

    insert first draf for frontend

--- fosscord-server/with-nginx/script.sh
+++ fosscord-server/with-nginx/script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 sed -i 's/placeholder/'$NGINX_HOST'/' /etc/nginx/sites-enabled/fosscord.conf
-certbot --nginx --agree-tos -m $MAIL_CERTBOT --domains $NGINX_HOST --non-interactive
+sed -i 's/placeholder_frontend/'$SERVER_URL'/' /etc/nginx/sites-enabled/fosscord.conf
+certbot --nginx --agree-tos -m $MAIL_CERTBOT --domains $NGINX_HOST,$SERVER_URL  --non-interactive --test-cert
 SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontabs/root > /dev/null
 nginx -s stop
 nginx -g "daemon off;"
